b890bec6474bfe83aad2d2910bd88335febf7601
Patricia <patricia.femenias@gmail.com>
2023-10-30 21:39:23 +0100

    Fix duplicated BrowserRouter

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,18 +1,16 @@
 import Header from "../Header/Header";
 import AppStyled from "./AppStyled";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 const App = (): React.ReactElement => {
   return (
     <>
       <AppStyled>
         <Header />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" />
-            <main className="main-content"></main>
-          </Routes>
-        </BrowserRouter>
+        <main className="main-content"></main>
+        <Routes>
+          <Route path="/" />
+        </Routes>
       </AppStyled>
     </>
   );
