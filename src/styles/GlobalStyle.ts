4929a6538be4d0ea6dab27b88d63ff20722112cf
Adri Cruzado <adricruzado96@gmail.com>
2023-10-30 17:20:00 +0100

    Add globals and resets to the stylesheet

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  *, ::before, ::after {
+    box-sizing: border-box;
+  }
+
+  html {
+    font-family: ${({ theme }) => theme.typography.mainFont};
+  }
+
+  body {
+    margin: 0;
+    font-size: 1rem;
+    background-color: ${({ theme }) => theme.colors.main};
+  }
+
+  ul, li {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  dl, dd {
+    margin: 0;
+    padding: 0;
+  }
+
+  button {
+    cursor: pointer;
+    font: inherit;
+    color: inherit;
+    background: none;
+    border: none;
+  }
+
+  img {
+    max-width: 100%
+  }
+`;
+
+export default GlobalStyle;
