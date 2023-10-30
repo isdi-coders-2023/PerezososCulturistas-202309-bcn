331c0d6499d7e7ebf412421f8218c39ddfceb213
Adri Cruzado <adricruzado96@gmail.com>
2023-10-30 17:13:37 +0100

    Add styles types

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,15 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    colors: {
+      main: string;
+      accent: string;
+      light: string;
+    };
+    typography: {
+      mainFont: string;
+      secondaryFont: string;
+    };
+  }
+}
