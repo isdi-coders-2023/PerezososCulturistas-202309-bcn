6f4933dfaef5724292cc93aacce13b5a278ad105
Patricia <patricia.femenias@gmail.com>
2023-10-31 17:31:32 +0100

    Add loadCharacters to CharacterStructure interface

--- src/features/characters/store/types.ts
+++ src/features/characters/store/types.ts
@@ -2,6 +2,7 @@ import { CharacterStructure } from "../types";
 
 interface CharacterContextStructure {
   characters: CharacterStructure[];
+  loadCharacters: (characters: CharacterStructure[]) => void;
 }
 
 export default CharacterContextStructure;
