4a74868c055e99b25f12a2874266747f172e1f23
Francisco Grande <fjgrandv@gmail.com>
2023-10-31 09:52:54 +0100

    Add component Navigation in component Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,16 @@
+import Navigation from "../Navigation/Navigation";
+import HeaderStyled from "./HeaderStyled";
+
 const Header = (): React.ReactElement => {
   return (
-    <img
-      className="header-logo"
-      src="/images/logo.webp"
-      alt="DragonBall logo"
-    ></img>
+    <HeaderStyled>
+      <img
+        className="header-logo"
+        src="/images/logo.webp"
+        alt="DragonBall logo"
+      ></img>
+      <Navigation />
+    </HeaderStyled>
   );
 };
 export default Header;
