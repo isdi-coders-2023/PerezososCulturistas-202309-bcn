9af3cc2a6219d23b44a79063f28faf4db652a0e2
Francisco Grande <fjgrandv@gmail.com>
2023-10-31 13:24:29 +0100

    Add container HeaderStyled and Navigation component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,18 @@
+import Navigation from "../Navigation/Navigation";
+import HeaderStyled from "./HeaderStyled";
+
 const Header = (): React.ReactElement => {
   return (
-    <img
-      className="header-logo"
-      src="/images/logo.webp"
-      alt="DragonBall logo"
-      width={343}
-      height={125}
-    ></img>
+    <HeaderStyled>
+      <img
+        className="header-logo"
+        src="/images/logo.webp"
+        alt="DragonBall logo"
+        width={343}
+        height={125}
+      ></img>
+      <Navigation />
+    </HeaderStyled>
   );
 };
 export default Header;
