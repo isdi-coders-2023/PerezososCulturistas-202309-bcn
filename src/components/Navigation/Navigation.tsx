22281dec90718b9ad42ab5669880ba9aeed14e64
Francisco Grande <fjgrandv@gmail.com>
2023-10-31 09:44:41 +0100

    Add Navigation component

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,14 @@
+import NavigationStyled from "./NavigationStyled";
+
+const Navigation = (): React.ReactElement => {
+  return (
+    <NavigationStyled>
+      <ul>
+        <li>HOME</li>
+        <li>CREATE</li>
+      </ul>
+    </NavigationStyled>
+  );
+};
+
+export default Navigation;
