e3cf414f10fe3b3dbbdae36e759bfcb9c495dc7c
Patricia <patricia.femenias@gmail.com>
2023-10-31 16:34:39 +0100

    Fix test arrange

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -5,10 +5,10 @@ import Header from "./Header";
 describe("Given a Header component", () => {
   describe("When it renders", () => {
     test("Then it should show a DragonBall logo", () => {
-      const altText = "DragonBall logo";
+      const expectedAltText = "DragonBall logo";
 
       render(<Header />);
-      const headerImage = screen.getByAltText(altText);
+      const headerImage = screen.getByAltText(expectedAltText);
 
       expect(headerImage).toBeInTheDocument();
     });
