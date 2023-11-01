8b2d829a3fd6d25a62c9a1d5df6d6f850930f25e
Adri Cruzado <adricruzado96@gmail.com>
2023-11-01 19:51:48 +0100

    Add CharactersList styles

---
+++ src/components/CharactersList/CharactersListStyled.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const CharactersListStyled = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 25px;
+`;
+
+export default CharactersListStyled;
