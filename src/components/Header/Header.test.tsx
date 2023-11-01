e3b3e7e00eca96f14e6227a5485522f8ac095a68
Francisco Grande <fjgrandv@gmail.com>
2023-11-01 11:10:24 +0100

    Add router in test

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,13 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Header from "./Header";
+import { BrowserRouter } from "react-router-dom";
 
 describe("Given a Header component", () => {
   describe("When it renders", () => {
     test("Then it should show a DragonBall logo", () => {
       const expectedAltText = "DragonBall logo";
 
-      render(<Header />);
+      render(
+        <BrowserRouter>
+          <Header />;
+        </BrowserRouter>,
+      );
       const headerImage = screen.getByAltText(expectedAltText);
 
       expect(headerImage).toBeInTheDocument();
