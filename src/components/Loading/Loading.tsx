a75538287e84ec2dc2dc202d18930270c42fedf8
Patricia <patricia.femenias@gmail.com>
2023-11-02 21:17:06 +0100

    Add LoadingStyled

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,13 +1,17 @@
+import LoadingStyled from "./LoadingStyled";
+
 const Loading = (): React.ReactElement => {
   return (
-    <div className="loading">
-      <img
-        className="loading--gif"
-        src="/images/goku-loading.gif"
-        alt="Goku playing with Dragon Balls"
-      />
-      <span className="loading--feedback">Loading...</span>
-    </div>
+    <LoadingStyled>
+      <div className="loading">
+        <img
+          className="loading--gif"
+          src="/images/goku-loading.gif"
+          alt="Goku playing with Dragon Balls"
+        />
+        <span className="loading--feedback">Loading...</span>
+      </div>
+    </LoadingStyled>
   );
 };
 
