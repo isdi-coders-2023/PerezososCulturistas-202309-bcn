1c2e356e17c5001b21bb4dd8e130942d46398d59
Patricia <patricia.femenias@gmail.com>
2023-11-03 10:21:18 +0100

    Get API characters

--- src/main.tsx
+++ src/main.tsx
@@ -7,14 +7,17 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./components/App/App";
 import GlobalStyle from "./styles/GlobalStyle";
 import mainTheme from "./styles/mainTheme";
+import CharactersProviderWrapper from "./features/characters/store/CharactersProviderWrapper";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ThemeProvider theme={mainTheme}>
-        <GlobalStyle />
-        <App />
-      </ThemeProvider>
+      <CharactersProviderWrapper>
+        <ThemeProvider theme={mainTheme}>
+          <GlobalStyle />
+          <App />
+        </ThemeProvider>
+      </CharactersProviderWrapper>
     </BrowserRouter>
   </React.StrictMode>,
 );
